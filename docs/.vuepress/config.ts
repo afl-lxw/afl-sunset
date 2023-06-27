786c87977d4d62582d60cce4289baf0e19793485
af-liu <18264480900@163.com>
2023-06-27 23:55:48 +0800

    feat: 路由增加

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from "vuepress/config";
 module.exports = defineConfig({
   title: '一斤阳夕',
   description: 'A pound of sunset',
-  themeConfig: {
-    repo: "vuejs/vuepress",
-    editLinks: true,
-    docsDir: "packages/docs/docs"
-  },
   plugins: {
     "@vuepress/pwa": {
       serviceWorker: true
@@ -23,5 +18,26 @@ module.exports = defineConfig({
         '@': 'docs/.vuepress/public'
       }
     }
-  }
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Posts', link: '/posts/' },
+      { text: 'About', link: '/about/' },
+    ],
+    sidebar: {
+      '/posts/': [
+        {
+          title: 'Posts',
+          collapsable: false,
+          children: [
+            '',
+            'post1',
+            'post2',
+          ]
+        }
+      ]
+    },
+    lastUpdated: 'Last Updated',
+  },
 }) 
